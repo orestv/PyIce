249ea14c4a1ab06dea09928776cbcee5a6eedacc
Orest Voloshchuk <orest.v@gmail.com>
2010-09-27 00:35:40 +0300

    first commit

---
+++ source.py
@@ -0,0 +1,141 @@
+#coding: utf-8
+import os
+from mutagen.easyid3 import EasyID3
+import random
+import shout
+import server
+import threading
+
+
+class Source:
+    def __init__(self, path='.'):
+        self.s = Server(path)
+        self.s.start()
+
+    def stop(self):
+        self.s.stop()
+
+
+
+class Server(threading.Thread):
+    _playlist_size = 15
+    _current_song = None
+
+    def __init__(self, path):
+        threading.Thread.__init__(self)
+        self._listener = server.Listener(self)
+        self._listener.start()
+        path = unicode(path)
+        self._songs = find_all_music_files(path)
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+    def run(self):
+        self._last_artists = []
+        self._playlist = []
+        song = random.choice(self._songs)
+        self._playlist.append(song)
+        self._next_song()
+        for i in range(self._playlist_size):
+            self._playlist.append(self._pick_new_song())
+        self.play()
+
+    def play(self):
+        s = shout.Shout()
+        s.host = 'localhost'
+        s.format = 'mp3'
+        s.password = 'ma$tercard'
+        s.mount = 'pyrockHQ'
+        s.name = 'Radio Seth'
+        s.genre = 'Rock'
+        s.url = 'http://213.130.28.169:8000/rock'
+        s.open()
+
+        bufsize = 4096
+
+        while 1:
+            print '---------------------------------------------'
+            for song in self._playlist:
+                print song
+            f = open(self._current_song)
+            artist = get_tag(self._current_song, 'artist')
+            if artist:
+                artist = artist.encode('utf-8')
+            title = get_tag(self._current_song, 'title')
+            if title:
+                title = title.encode('utf-8')
+            meta = artist + ' - ' + title
+            s.set_metadata({'song' : meta})
+            nbuf = f.read(bufsize)
+            while 1:
+                if self.stopped():
+                    print 'Server stopped!'
+                    f.close()
+                    s.close()
+                    self._listener.stop()
+                    return
+                buf = nbuf
+                nbuf = f.read(bufsize)
+                if not buf:
+                    break
+                s.send(buf)
+                s.sync()
+            f.close()
+            self._next_song()
+
+        s.close()
+
+    def _next_song(self):
+        self._current_song = self._playlist[0]
+        artist = get_tag(self._current_song, 'artist')
+        if artist:
+            self._last_artists.append(artist)
+            if self._last_artists and len(self._last_artists) > self._playlist_size:
+                self._last_artists.pop(0)
+        self._playlist.pop(0)
+        self._playlist.append(self._pick_new_song())
+
+    def _pick_new_song(self):
+        artists = [get_tag(f, 'artist') for f in self._playlist]
+        artists.extend(self._last_artists)
+        choice = None
+        while not choice:
+            choice = random.choice(self._songs)
+            artist = get_tag(choice, 'artist')
+            if not artist or artist in artists:
+                choice = None
+        return choice
+
+    def get_playlist(self):
+        return self._playlist
+
+def get_tag(path, tagname):
+   try:
+       tags = EasyID3(path)
+   except mutagen.id3.ID3NoHeaderError:
+       return None
+   if tags.has_key(tagname):
+       artists = tags[tagname]
+       if artists:
+           return artists[0]
+       else:
+           return None
+   else:
+       return None
+
+def find_all_music_files(top, type = 'mp3'):
+    list = [(p, f) for (p, _, f) in os.walk(top) if f]
+
+    result = []
+    for (path, files) in list:
+        for file in files:
+            if file.endswith(type):
+                result.append(os.path.join(path, file))
+    return result
+#    print (parent, dirs, files)
+
