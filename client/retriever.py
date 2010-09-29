8944323207b2f9ceec24b4ca4492096bd71f5bba
Orest Voloshchuk <orest.v@gmail.com>
2010-09-29 22:31:21 +0300

    Fixing up network part

--- client/retriever.py
+++ client/retriever.py
@@ -1,9 +1,9 @@
 import socket
 
 class Retriever:
-    def __init__(self, host='localhost', port='50000'):
+    def __init__(self, host='localhost', port=50000):
         self._host = host
-        self._port = port
+        self._port = int(port)
 
     def connect(self):
         self._s = open_socket(self._host, self._port)
@@ -11,7 +11,29 @@ class Retriever:
     def disconnect(self):
         self._s.close()
 
+    def get_playlist(self):
+        pl = send(self._s, 'playlist', True)
+        print pl
+
 def open_socket(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     return s
+
+def send(socket, command, bReply = False):
+    socket.send(command)
+    if bReply:
+        socket.settimeout(3)
+        data = None
+        i = i + 1
+        try:
+            data = socket.recv(BUFFER_SIZE)
+        except:
+            print 'Timeout'
+            return data
+        return data
+    else:
+        return None
+
+
+
