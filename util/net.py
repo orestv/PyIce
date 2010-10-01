e984d66d3f39a3088c8a894ed2ccdce1bed0ae13
Orest Voloshchuk <orest.v@gmail.com>
2010-10-01 20:18:56 +0300

    fixed up networking listening again

--- util/net.py
+++ util/net.py
@@ -1,24 +1,39 @@
 import socket
 import threading
 
-def receive(socket, bufsize=4096, fStopped=None):
+def receive(socket, fStopped=None, bClose=True, bufsize=4096):
     if fStopped:
         socket.settimeout(1)
     result = ''
+    data = None
     while 1:
         try:
+            print 'Trying to receive...'
             data = socket.recv(bufsize)
+            print 'Data received!'
             if not data:
-                socket.close()
+                if bClose:
+                    socket.close()
                 return result
             else:
                 result += data
-        except AttributeError, e:
+                data = None
+        except:
             if fStopped and fStopped():
-                socket.close()
-                return result+data
+                    if bClose:
+                        socket.close()
+                    return result
+            if result:
+                if data:
+                    result += data
+                    data = None
+                    continue
+                else:
+                    if bClose:
+                        socket.close()
+                    return result
 
-def send(socket, data, fStopped=None):
+def send(socket, data, fStopped=None, bClose=True):
     if fStopped:
         socket.settimeout(1)
     total = 0
@@ -28,9 +43,15 @@ def send(socket, data, fStopped=None):
             sent = socket.send(data[total:])
             total += sent
             if sent == size:
-                socket.close()
+                if bClose:
+                    socket.close()
                 return True
         except AttributeError, e:
             if fStopped and fStopped():
-                socket.close()
+                if bClose:
+                    socket.close()
                 return False
+
+def command(socket, command, fStop=None):
+    send(socket, command, fStop, False)
+    return receive(socket, fStopped=fStop)
